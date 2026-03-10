fe4ac9c991157cbf369265a508f9000621f737f4
haidarteray <htteray5253@gmail.com>
2026-03-10 16:55:18 +0100

    Add files via upload

---
+++ magic.js
@@ -0,0 +1,6 @@
+// magic.js - Simplifié
+// Désactivation des effets complexes pour une meilleure stabilité.
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio chargé avec succès.');
+});
