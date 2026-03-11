a1e28bdcbe91b0825d075843949c70e91759e069
haidarteray <htteray5253@gmail.com>
2026-03-11 23:32:07 +0100

    Delete magic.js

--- magic.js
+++
@@ -1,6 +0,0 @@
-// magic.js - Simplifié
-// Désactivation des effets complexes pour une meilleure stabilité.
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio chargé avec succès.');
-});
